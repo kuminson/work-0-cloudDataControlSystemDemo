2fcfcdd377621fedd6a37e4e74af18ac8d1e5a89
kumin <273423480@qq.com>
2016-09-29 09:34:33 +0800

    create object
    create base filed for new object

---
+++ resource/ie/html5shiv.js
@@ -0,0 +1,9 @@
+// html页面header中引用内容如下
+//  <!--[if IE]>
+// <script src="js/html5shiv.js"></script>
+//  <![endif]-->
+
+//在css reset中引用如下内容
+//article,aside,dialog,footer,header,section,footer,nav,figure,menu{display:block;}
+
+(function(){if(!/*@cc_on!@*/0)return;var e ="abbr,article,aside,audio,canvas,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
